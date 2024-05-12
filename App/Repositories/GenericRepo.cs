2edefd211be426e4d3f65e1a9234d2c082f08e1b
Kohage <89829621077@bk.ru>
2024-05-12 10:44:52 +0300

    Fix

---
+++ App/Repositories/GenericRepo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IspoQueue.App.Repositories;
+
+public class GenericRepo<TEntity> : IGenericRepo<TEntity> where TEntity : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepo(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task Create(TEntity item)
+    {
+        await _dbSet.AddAsync(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<TEntity?> FindById(Guid id)
+    {
+        var layers = await Get();
+        var e = await _dbSet.FindAsync(id);
+
+        return e;
+    }
+
+    public async Task<List<TEntity>?> Get()
+    {
+        var l = await _dbSet.AsNoTracking().ToListAsync();
+
+        return l;
+    }
+
+    public async Task Remove(TEntity item)
+    {
+        _dbSet.Remove(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(TEntity item)
+    {
+        _context.Entry(item).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ App/Repositories/IGenericRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace IspoQueue.App.Repositories;
+
+public interface IGenericRepo<TEntity> where TEntity : class
+{
+    Task Create(TEntity item);
+    Task<TEntity?> FindById(Guid id);
+    Task<List<TEntity>?> Get();
+    Task Remove(TEntity item);
+    Task Update(TEntity item);
+}
