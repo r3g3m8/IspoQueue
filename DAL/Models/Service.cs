797bc008dc7216ff58b603c920c027fff39d2cfd
Kohage <89829621077@bk.ru>
2024-05-13 23:18:29 +0300

    Add methods to queue controller

--- DAL/Models/Service.cs
+++ DAL/Models/Service.cs
@@ -2,5 +2,6 @@ namespace IspoQueue.DAL.Models;
 
 public class Service {
     public int Id { get; set; }
-    public string? Name { get; set; } 
+    public string? Name { get; set; }
+    public string? IdentityStr { get; set; }
 }
